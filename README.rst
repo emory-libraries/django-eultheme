fbd428fa504b9e4e420f659509b907b656855b93
rlskoeser <rebecca.s.koeser@emory.edu>
2014-04-21 16:41:25 -0400

    preliminary import of advanced search filters from ddisearch

--- README.rst
+++ README.rst
@@ -20,6 +20,8 @@ files from app directories (included in the default settings).
 Recommended: setup a context processor to include your project version
 in all templates as **SW_VERSION**.
 
+Include **eultheme.js** in your site base if you use any of the template snippets
+that require it (e.g., advanced search filters).
 
 **NOTE**: We use git flow naming conventions for this git repository.
 New development will always be in in *develop* and the most recent
---
+++ eultheme/static/eultheme/js/eultheme.js
@@ -0,0 +1,41 @@
+$(document).ready(function(){
+
+    /* advanced search filter controls */
+    function toggleGroup(elem){
+        var $this = $(elem),
+        group = $this.attr('data-type');
+
+        $this.toggleClass('active');
+        $(".group[data-type='"+ group +"']").stop(false,true).slideToggle(500);
+
+    }
+
+    var $toggleSwitch = $('.toggle.switch');
+    if($toggleSwitch.length>0){
+        $toggleSwitch.on('click',function(e){
+            e.preventDefault();
+            toggleGroup(this);
+        });
+    }
+
+    $advOptionsGroup =  $(".adv.group");
+
+    $('.adv.group .controls .btn').bind('click', function(e){
+        e.preventDefault();
+
+        var $this = $(this);
+        if($this.hasClass('submit')){
+            search.get();
+        }
+        else if($this.hasClass('reset')){
+            reset($advOptionsGroup);
+        }
+    });
+
+    function reset(elem){
+        var $elem = $(elem),
+        $inputs = $elem.find('input');
+        $inputs.attr('value','').val('');
+    }
+
+});
