ef21c8a9863019729e19d3258eab855def7ec2d3
Kevin Glover <hkglover@gmail.com>
2015-06-22 15:33:19 -0400

    Refactored client ip method to class method
    - Cleaned up middleware get_client_ip method
    - Renamed method to client_ip

--- eultheme/middleware.py
+++ eultheme/middleware.py
@@ -5,20 +5,25 @@ from downtime.middleware import DowntimeMiddleware
 class DownpageMiddleware(DowntimeMiddleware):
 
     def process_request(self, request):
-        def get_client_ip(request):
-            x_forwarded_for = request.META.get('HTTP_X_FORWARDED_FOR')
-            if x_forwarded_for:
-                ip = x_forwarded_for.split(',')[0]
-            else:
-                ip = request.META.get('REMOTE_ADDR')
-            return ip
 
         downtime_request = super(DownpageMiddleware,self).process_request(request)
 
         allowed_ips = getattr(settings, 'DOWNTIME_ALLOWED_IPS', [])
 
         if downtime_request and allowed_ips and \
-            get_client_ip(request) in allowed_ips:
+            self.client_ip in allowed_ips:
             return None
         else:
             return downtime_request
+
+    @classmethod
+    def client_ip(request):
+        """
+        Returns the client IP address from the request.
+        """
+        x_forwarded_for = request.META.get('HTTP_X_FORWARDED_FOR')
+        if x_forwarded_for:
+            ip = x_forwarded_for.split(',')[0]
+        else:
+            ip = request.META.get('REMOTE_ADDR')
+        return ip
